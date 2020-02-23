5bdafc13e959d9585df9bbea469522a0d5b551db
Jose <joses8860@gmail.com>
2020-02-23 14:29:51 -0800

    team classes

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string name;
+    public int hitting;
+    public int fielding;
+    public int speed;
+    public int control;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/scripts/Team.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public struct maneuvers
+    {
+        public Player JocPeterman;
+        public Player CodyBellighter;
+        public Player MookieBites;
+        public Player JustinTruman;
+        public Player WillSmite;
+        public Player GavinLuxury;
+        public Player MaxMunchies;
+        public Player CoreySinger;
+        public Player WalkerButtler;
+    }
+
+    public struct saviors
+    {
+        public Player MikeTuna;
+        public Player JustinUpdog;
+        public Player AlbertPulledpork;
+        public Player AnthonyRedome;
+        public Player AndreltonSimptoms;
+        public Player TommyLaStellar;
+        public Player JasonCastiron;
+        public Player ShoheiOtonniH;
+        public Player ShoheiOtonniP;
+    }
+}
